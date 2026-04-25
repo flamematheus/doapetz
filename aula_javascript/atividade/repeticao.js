679f38e1c3a1ca637ae4755ae1a680e72fd05128
Matheus Alves <matheustz209@gmail.com>
2026-04-24 21:46:45 -0300

    att

--- aula_javascript/atividade/repeticao.js
+++ aula_javascript/atividade/repeticao.js
@@ -120,7 +120,7 @@ do {
     }
 } while (opcao !== "0");
 
-*/
+
 
 // jogo adivinha o numero 
 
@@ -148,8 +148,130 @@ do {
      }else{
          console.log(`acertou o numero secreto é : ${numerosecreto}`)
 
+          console.log(`precisou de ${tentativas}tentativas !`)
+
+
      }
 } while (palpite !== numerosecreto);
 
 
 
+
+//for 
+console.log("Comtando de 1 a 10 :\n")
+for (let i = 1;i <=10; i++) {
+    console.log(`numero :${i}`)  
+}
+
+//tabuada 
+
+const numero = Number(prompt("tabuada de qual numero ?"))
+if (Number.isNaN(numero)) {
+    console.log("numero invalido")
+    
+} else {
+    console.log(`\n tabuada do ${numero}\n`);
+    for (let i = 1; i<=10; i++){
+        const resultado = numero * i;
+        console.log(`${numero}x ${i}= ${resultado}`);
+
+    }      
+}
+
+
+//numeros pares (0 -20)
+
+console.log("Comtando de 0 a 20 :\n")
+for (let i = 0;i <=20 ; i+=2) {
+    console.log(`numero :${i}`)  
+}
+
+
+
+console.log("Comtando de 0 a 20 :\n")
+for (let i = 1000;i >=0 ; i-=2) {
+    console.log(`numero :${i}`)  
+}
+
+ 
+const altura = 5;
+for(let linha =1; linha <= altura; linha ++){
+    let asteriscos = "";
+
+    for (let i  = 1 ; i <= linha ; i ++){
+        asteriscos += "*"
+    }
+ console.log(asteriscos);
+}
+
+ // array(lista)
+
+const frutas =["maçã","banana", "laranja","uva"];
+ 
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas[2]);
+console.log(frutas[3]);
+
+console.log(frutas.length);
+
+// for of 
+for (const fruta of frutas){
+    console.log(`-${fruta}`)
+}
+
+const numeros = [10,25,8,42,15,7,];
+
+let soma = 0
+for (const numero of numeros){
+     soma +=numero ;
+}
+
+//objeto
+
+const pessoa = {
+    nome : "lucas",
+    idade : 35,
+    cidade:"santo andre",
+    profissao:"professor "
+}
+console.log(pessoa.nome)
+console.log(pessoa["idade"])
+
+// for in 
+for (const chave in objeto){
+
+}
+
+const aluno = {
+    nome : "maria silva",
+    matricula:"20223401",
+    curso: "deseanvolvedor web",
+    nota :9.5,
+}
+console.log("Dados do aluno :\n")
+for(const chave in aluno){
+    const valor = aluno[chave]
+    console.log(`${chave}:${valor}`)
+}
+*/
+//estoque de produtos
+const estoque ={
+    "notebook ":15,
+    "mause":50,
+    "teclado":30,
+    "monitor":8,
+    "webcam":22
+
+}
+console.log("estoque :")
+for (const produto in estoque ){
+    const quantidade= estoque[produto];
+    if (quantidade <10) {
+        console.log(`${produto}:${quantidade}unidade (estoque baixo)`)
+        
+    } else {
+        console.log(`${produto}:${quantidade} quantidade de unidade normal `)
+        
+    }
+}
